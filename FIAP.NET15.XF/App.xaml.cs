f409b4175a78105c955bfcd0ef6ff09466023ac7
BrunoLSouza <51425201+BrunoLSouza@users.noreply.github.com>
2020-03-02 08:35:04 -0300

    up manual parte 1

---
+++ FIAP.NET15.XF/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using FIAP.NET15.XF.Services;
+using FIAP.NET15.XF.Views;
+
+namespace FIAP.NET15.XF
+{
+    public partial class App : Application
+    {
+
+        public App()
+        {
+            InitializeComponent();
+
+            DependencyService.Register<MockDataStore>();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
